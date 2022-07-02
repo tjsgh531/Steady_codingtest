1447214ec54b4c1b7387e3dedc4ffe5fd7b8d3d5
Sun Ho <tjsgh5312@daum.net>
2022-07-03 08:54:07 +0900

    파란색 구슬은 check하면 안된다

--- 구슬탈출2/solution.py
+++ 구슬탈출2/solution.py
@@ -3,4 +3,14 @@ N, M = map(int, input().split())
 graph = [list(input().split()) for _ in range(N)]
 
 def DFS(x, y);
-    
+    check = [[True for _ in range(M)]]
+    row = [True]
+    for i in range(1, N-1):
+        row = [True]
+        for j in range(1, M-1):
+            row.append(False)
+        row.append(True)
+        check.append(row)
+
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
