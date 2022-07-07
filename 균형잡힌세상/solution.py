32e2c207bc6959a00a9d411fcf212074e904801e
Sun Ho <tjsgh5312@daum.net>
2022-07-07 23:39:50 +0900

    와 왜 ? 겁나 쉬운문제인데 왜 못풀지??

---
+++ 균형잡힌세상/solution.py
@@ -0,0 +1,32 @@
+
+sen = []
+while 1:
+    temp = input()
+    if temp =='.':
+        break
+    else:
+        sen.append(temp)
+
+for sentence in sen:
+    small_stack = 0
+    big_stack = 0
+    for word in sentence:
+        if word == '(':
+            small_stack += 1
+        elif word == ')':
+            small_stack -= 1
+            if small_stack < 0:
+                print("no")
+                break
+        elif word == '[':
+            big_stack += 1
+        elif word == ']':
+            big_stack -= 1
+            if big_stack < 0:
+                print("no")
+                break
+    else:
+        if small_stack == 0 and big_stack == 0:
+            print("yes")
+        else:
+            print("no")
