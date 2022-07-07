21c84f507539f3d9de76b8c6824beba56e8acc6f
Sun Ho <tjsgh5312@daum.net>
2022-07-08 08:52:15 +0900

    습관!!!

--- 균형잡힌세상/answer.py
+++ 균형잡힌세상/answer.py
@@ -1,14 +1,17 @@
 import sys
+#습관 들입시다
 input = sys.stdin.readline
 
 while 1:
     string = input().rstrip()
+    #rstrip() 
     stack = []
     true_flag = 1
     for cha in string:
         if cha == '(' or cha == '[':
             stack.append(cha)
         elif cha == ')':
+            # if len(stack) == 0 => if stack
             if stack and stack[-1] == '(':
                 stack.pop()
             else:
