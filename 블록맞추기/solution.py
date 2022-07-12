2ee1cf87a2410af10741b00518c8e77bdcd3b281
Sun Ho <tjsgh5312@daum.net>
2022-07-12 13:52:05 +0900

    type 구분을 어캐 하냐..

--- 블록맞추기/solution.py
+++ 블록맞추기/solution.py
@@ -1,32 +1,67 @@
 from collections import deque
 
-def findshape(row, col):
-    colsize = 0
-    row_box_size = 0
-    row_block_size = 0
-
-    colcheck = False
-    rowcheck = False
-
+#
+def typeSort(row, col):
+    iszero = False
+    isactive = False
     cur_pos = (row, col)
-    #row size check
-    while not rowcheck and board[cur_pos[0], cur_pos[1]] == 0:
-        row_box_size += 1
-        checked[cur_pos[0]][cur_pos[1]] = True
-        cur_pos = (cur_pos[0], cur_pos[1] + 1)
-        if board[cur_pos[0]][cur_pos[1]] == 1:
-            rowcheck = True
-        else:
-            row_block_size += 1
+
+    box_size = 0
+    zero_size = 0
+
+    #c타입 구분 못하겠는데 ...
+    while not (isactive and board[cur_pos[0]][cur_pos[1]] == 0):
+        box_size += 1
+
+    #zero_size = 0이면 A B D형태, zero_size가 값이 있으면 C => C형 구분 가능
+    #board[row+1][col] == 1 이면 A B 형태, 아니면 D => D형태 구분 가능
+    #board[row+1][col + box_size -1] == 0이면 A 형태, 아니면 B => A, B 구분가능
+    if zero_size != 0:
+
+        return 'C', box_size
+    if board[row+1][col] != 1:
+        return 'D', box_size
+    if board[row+1][col + box_size -1] == 0:
+        return 'A', box_size
+    else:
+        return 'B', box_size
+
+def checkShape(type, boxsize, row, col):
+    box_height = 0
+    if type == 'A':
+        # x_size check
+        cur_pos = (row+1, col)
+        x_size = 0
+        while board[cur_pos[0]][cur_pos[1]] == 1:
+            x_size += 1
+            box_height += 1
+            cur_pos = (cur_pos[0] + 1, cur_pos[1])
+        if x_size != x:
+            return False
+        # w & y size check
+        cur_pos = (row, col)
+        w_size = 0
+        y_size = 0
+        isactive = False
+        while not (isactive and board[cur_pos[0]][cur_pos[1]] == 0):
+            if 
+
+
+    
 
 N = int(input())
 u, v, w, x, y = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-checked = [[False for _ in range(N)] for _ in range(N)]
+visited = [[False for _ in range(N)] for _ in range(N)]
 
 temp_ans = (0, 0)
 for i in range(N):
     for j in range(N):
-        if board[i][j] == 1 and checked[i][j] == False:
-            findshape(i, j)
+        if board[i][j] == 1 :
+            box_type, box_width = typeSort(i, j)
+            # u값이 안맞는 경우
+            if box_type != 'D' and box_width != u:
+                continue
+    
+
 
