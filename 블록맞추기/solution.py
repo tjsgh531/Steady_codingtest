27215cc2829a2cffd98dce19c245e2ce18489be3
Sun Ho <tjsgh5312@daum.net>
2022-07-12 14:12:51 +0900

    와 이건 노가다 하라면 할 수 있겠는데 와.. 해야해?

--- 블록맞추기/solution.py
+++ 블록맞추기/solution.py
@@ -1,53 +1,50 @@
 from collections import deque
 
-#
-def typeSort(row, col):
-    iszero = False
-    isactive = False
+#box_size부터 구해야겠다
+def boxSize(row, col):
+    q = deque()
+    maxrow = row
+    maxcol = col
+
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+
+    q.append((row, col))
+    while q:
+        y, x = q.popleft()
+        visited[y][x] = True
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= N or ny <0 or ny >= N:
+                continue
+            if board[ny][nx] == 1:
+                maxrow = max(ny, maxrow)
+                maxcol = max(nx, maxcol)
+                q.append((ny, nx))
+    return maxrow, maxcol
+
+def checkSize(row, col, maxrow, maxcol):
+    box_width = maxcol - col + 1
+    box_height = maxrow - row + 1#
+
+    #타입 구분 부터
     cur_pos = (row, col)
+    c_sort = 0
+    while board[cur_pos[0]][cur_pos[1]] == 1:
+        c_sort += 1
+        cur_pos = (cur_pos[0], cur_pos[1] + 1)
+    if c_sort == maxrow - row +1:
 
-    box_size = 0
-    zero_size = 0
-
-    #c타입 구분 못하겠는데 ...
-    while not (isactive and board[cur_pos[0]][cur_pos[1]] == 0):
-        box_size += 1
-
-    #zero_size = 0이면 A B D형태, zero_size가 값이 있으면 C => C형 구분 가능
-    #board[row+1][col] == 1 이면 A B 형태, 아니면 D => D형태 구분 가능
-    #board[row+1][col + box_size -1] == 0이면 A 형태, 아니면 B => A, B 구분가능
-    if zero_size != 0:
-
-        return 'C', box_size
-    if board[row+1][col] != 1:
-        return 'D', box_size
-    if board[row+1][col + box_size -1] == 0:
-        return 'A', box_size
-    else:
-        return 'B', box_size
-
-def checkShape(type, boxsize, row, col):
-    box_height = 0
-    if type == 'A':
-        # x_size check
-        cur_pos = (row+1, col)
-        x_size = 0
-        while board[cur_pos[0]][cur_pos[1]] == 1:
-            x_size += 1
-            box_height += 1
-            cur_pos = (cur_pos[0] + 1, cur_pos[1])
-        if x_size != x:
+    else: #C type
+        if box_width != u:
             return False
-        # w & y size check
-        cur_pos = (row, col)
-        w_size = 0
-        y_size = 0
-        isactive = False
-        while not (isactive and board[cur_pos[0]][cur_pos[1]] == 0):
-            if 
-
-
-    
+        else:
+            if c_sort != u - (w+y):
+                return False
+            else:
 
 N = int(input())
 u, v, w, x, y = map(int, input().split())
@@ -58,10 +55,7 @@ temp_ans = (0, 0)
 for i in range(N):
     for j in range(N):
         if board[i][j] == 1 :
-            box_type, box_width = typeSort(i, j)
-            # u값이 안맞는 경우
-            if box_type != 'D' and box_width != u:
-                continue
+            
     
 
 
